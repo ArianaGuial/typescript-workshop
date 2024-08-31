4fc2313f29a0db7de38af9439416563121c0abed
Marie Ariana Alexi B. Guial <158830223+ArianaGuial@users.noreply.github.com>
2024-08-31 20:52:25 +0800

    Update forEach.js

---
+++ src/forEach.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.processFruits = void 0;
+
+var processFruits = function (fruits) {
+    fruits.forEach(function (fruit) {
+        console.log(fruit.toUpperCase());
+    });
+};
+
+exports.processFruits = processFruits;
