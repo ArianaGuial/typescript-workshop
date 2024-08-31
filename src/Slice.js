2ac8fc535a823b4b8cf17916c9ac3755729e9374
Marie Ariana Alexi B. Guial <158830223+ArianaGuial@users.noreply.github.com>
2024-08-31 20:51:20 +0800

    Update Slice.js

---
+++ src/Slice.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getSubstring = void 0;
+
+var getSubstring = function (str, start, end) {
+    return str.slice(start, end);
+};
+
+exports.getSubstring = getSubstring;
