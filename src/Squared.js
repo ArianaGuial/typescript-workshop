d33013238cefe7b39b76c672486665509168c7cd
Marie Ariana Alexi B. Guial <158830223+ArianaGuial@users.noreply.github.com>
2024-08-31 20:51:37 +0800

    Update Squared.js

---
+++ src/Squared.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.squareNumbers = void 0;
+
+var squareNumbers = function (numbers) {
+    return numbers.map(function (number) { return number * number; });
+};
+
+exports.squareNumbers = squareNumbers;
