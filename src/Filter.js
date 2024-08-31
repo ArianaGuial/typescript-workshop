98485cf21cd9cc45310fe0a2ed4a1b6f4b77ec69
Marie Ariana Alexi B. Guial <158830223+ArianaGuial@users.noreply.github.com>
2024-08-31 20:51:07 +0800

    Update Filter.js

---
+++ src/Filter.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.filterWordsGreaterThanFive = void 0;
+
+var filterWordsGreaterThanFive = function (words) {
+    return words.filter(function (word) { return word > 5; });
+};
+
+exports.filterWordsGreaterThanFive = filterWordsGreaterThanFive;
